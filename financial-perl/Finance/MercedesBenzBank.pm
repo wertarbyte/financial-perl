8c2ad636dfdb1538659f5cbbe390e7edd6b118d1
Stefan Tomanek <stefan@pico.ruhr.de>
2008-12-31 10:13:35 +0000

    * see above

---
+++ financial-perl/Finance/MercedesBenzBank.pm
@@ -0,0 +1,82 @@
+package Finance::MercedesBenzBank;
+require Finance::GenericWebBot;
+require HTML::TreeBuilder;
+require Unicode::String;
+@ISA = ("Finance::GenericWebBot");
+
+use strict;
+
+our $start_url = "https://www.mercedes-benz-bank.de/";
+
+sub login {
+    my ($self) = @_;
+    my $m = $self->{mech};
+    $m->get($start_url);
+    $m->follow_link( text => "Login Online Banking");
+    $m->form_number(2);
+    $m->field( "username", $self->{credentials}{id} );
+    $m->field( "password", $self->{credentials}{pin} );
+    $m->click( '$$event_login' );
+    my $acc = $self->{credentials}{account};
+    #$m->follow_link( url_regex => qr{acountNo=$acc});
+    $m->follow_link( text_regex => qr{Tagesgeldkonto});
+}
+
+sub credentials {
+    my ($self, $id, $pin, $account) = @_;
+    $self->{credentials}{account} = $account if defined $account;
+
+    return $self->SUPER::credentials($id, $pin) &&
+           defined $account;
+}
+
+sub statements {
+    return ();
+}
+
+our %key_table = (
+    valutaDate => "booked",
+    description => "desc",
+    value  => "amount"
+);
+
+sub transactions {
+    my ($self, @trans) = @_;
+    my $m = $self->{mech};
+    $m->form_with_fields( "period" );
+    $m->set_visible( [ option => "48 Monate" ] );
+    $m->click( '$$event_refresh' );
+    $m->follow_link( text_regex => qr{Drucken} );
+    
+    my @book;
+
+    my $tree = new HTML::TreeBuilder;
+    $tree->parse($m->content);
+    my @rows = $tree->look_down( _tag => "tr", class => qr{(even|odd)-row} );
+    for my $r (@rows) {
+        my %t;
+        for my $c ( $r->look_down( _tag => "td" ) ) {
+            my $content = Unicode::String::latin1($c->as_trimmed_text)->utf8;
+            my ($key) = ($c->attr('headers') =~ m/^[^:]+:(.*)$/);
+            if (defined $key_table{$key}) {
+                if ($key eq "valutaDate") {
+                    $content =~ s/([0-9]{2})\.([0-9]{2})\.([0-9]{4})/\3-\2-\1/;
+                }
+                if ($key eq "description") {
+                }
+                if ($key eq "value") {
+                    # convert value to a true number
+                    $content =~ s/ EUR$//;
+                    $content =~ s/\.//g;
+                    $content =~ s/,/./g;
+                    $content *= 1;
+                }
+                $t{ $key } = $content;
+            }
+        }
+        push @book, $self->construct_transaction( $t{valutaDate}, $t{valutaDate}, $t{value}, $t{description} );
+    }
+    return \@book;
+}
+
+1;
